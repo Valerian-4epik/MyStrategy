b8459810687a24e2dc9e6caaae8550665dd6d4a2
Valerian-4epik <98113328+Valerian-4epik@users.noreply.github.com>
2023-01-29 23:45:32 +0300

    add new scriptrs

--- Assets/CodeBase/Constructions/BuildSlot.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine;
-
-namespace CodeBase.Constructions {
-    public class BuildSlot : MonoBehaviour{
-        
-    }
-}
---
+++ Assets/CodeBase/Constructions/Building.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+namespace CodeBase.Constructions {
+    public class Building : MonoBehaviour {
+        public bool Placed { get; private set; }
+        public BoundsInt area;
+    }
+}
